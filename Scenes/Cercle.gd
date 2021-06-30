8116147187da613923783377ff9874d7d3997e73
Ant0yne <antoine.soliv@gmail.com>
2021-06-30 16:00:45 +0200

    Fin spawn + anim cercle

--- Scenes/Cercle.gd
+++ Scenes/Cercle.gd
@@ -1,13 +1,50 @@
 extends AnimatedSprite
 
 onready var collisionShape2DCercle = $Area2D/CollisionShape2D
+onready var cercleDonnee = CercleDonnees
+
+func _process(delta):
+	if animation == "Idle Rempli" && cercleDonnee.ennemi_zero == true && cercleDonnee.nbre_vague == 1:
+		set_animation("Idle Abime 1")
+		play()
+
+	if animation == "Idle Abime 1" && cercleDonnee.ennemi_zero == true && cercleDonnee.nbre_vague == 2:
+		set_animation("Idle Abime 2")
+		play()
+	
+	if animation == "Idle Abime 2" && cercleDonnee.ennemi_zero == true && cercleDonnee.nbre_vague == 3:
+		set_animation("Idle Detruit")
+		play()
 
 func _on_Area2D_body_entered(body):
 	if animation == "Idle Vide":
 		set_animation("Remplissage")
 		play()
+		
+	if animation == "Idle Abime 1" && cercleDonnee.ennemi_zero == true:
+		set_animation("Eclairage")
+		play()
+		cercleDonnee.eclairage_joue = true
+		
+	if animation == "Idle Abime 2" && cercleDonnee.ennemi_zero == true:
+		set_animation("Eclairage")
+		play()
+		cercleDonnee.eclairage_joue = true
 
 func _on_Cercle_animation_finished():
 	if animation == "Remplissage":
 		set_animation("Eclairage")
+		play()
+		cercleDonnee.eclairage_joue = true
+	
+	if animation == "Eclairage" && cercleDonnee.nbre_vague == 0:
+		set_animation("Idle Rempli")
+		play()
+		
+	if animation == "Eclairage" && cercleDonnee.nbre_vague == 2:
+		set_animation("Idle Abime 1")
+		play()
+
+	if animation == "Eclairage" && cercleDonnee.nbre_vague == 3:
+		set_animation("Idle Abime 2")
 		play()
--- Script/GestionRH.gd
+++ Script/GestionRH.gd
@@ -1,25 +1,29 @@
 extends Node2D
 
+onready var cercle = CercleDonnees
+
 const RH = preload("res://Scenes/RH.tscn")
 
 var vecteur_cible
 var nbre_rh = int(6)
 var ajout_nbre_rh = int(3)
-var nbre_vague = int(1)
 
 func _ready():
 	_random_position()
 
 func _process(delta):
-	if Input.is_action_just_released("ui_select"):
-		nbre_vague += 1
-		nbre_rh += ajout_nbre_rh
+	if get_child_count() <= 0:
+		cercle.ennemi_zero = true
+	if cercle.eclairage_joue == true && get_child_count() <= 0 :
+		cercle.ennemi_zero = false
 		while get_child_count() <= nbre_rh:
 			var rh = RH.instance()
 			rh.global_position = vecteur_cible
 			add_child(rh)
 			_random_position()
+		cercle.eclairage_joue = false
+		nbre_rh += ajout_nbre_rh
+		cercle.nbre_vague += 1
 		
 func _random_position():
 	vecteur_cible = Vector2(rand_range(80, 1065), rand_range(110, 520))
-
