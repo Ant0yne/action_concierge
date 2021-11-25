2ef6cef41c41401264a50f08ac40bc3020b88449
Ant0yne <antoine.soliv@gmail.com>
2021-11-25 15:19:24 +0100

    peaufinage menu pause et option

--- Scenes/Menu Option.gd
+++ Scenes/Menu Option.gd
@@ -7,8 +7,11 @@ var reset_focus_bouton = bool(false)
 func _ready():
 	self.visible = false
 	reset_focus_bouton = false
-	
+
 func _process(delta):
+	_focus_manette()
+	
+func _focus_manette():
 	if self.visible == true and reset_focus_bouton == false:
 		get_node("Quitter").grab_focus()
 		reset_focus_bouton = true
--- Scenes/Menu Pause.gd
+++ Scenes/Menu Pause.gd
@@ -1,11 +1,14 @@
 extends Control
 
 onready var menuOption = $"Menu Option"
+onready var bouton = $Bouton
 
 var reset_focus_bouton = bool(false)
+var bouton_focus = str("Bouton/Reprendre")
 
 func _ready():
 	reset_focus_bouton = false
+	bouton_focus = "Bouton/Reprendre"
 
 func _process(delta):
 	if Input.is_action_just_released("ui_passer"):
@@ -15,15 +18,19 @@ func _process(delta):
 
 func _focus_manette():
 	if self.visible == true and reset_focus_bouton == false and menuOption.visible == false:
-		get_node("Reprendre").grab_focus()
+		get_node(bouton_focus).grab_focus()
 		reset_focus_bouton = true
+		bouton.visible = true
 	if self.visible == false and reset_focus_bouton == true:
 		reset_focus_bouton = false
+		bouton_focus = "Bouton/Reprendre"
 	if menuOption.visible == true and reset_focus_bouton == true:
 		reset_focus_bouton = false
+		bouton_focus = "Bouton/Options"
 
 func _on_Options_button_up():
 	menuOption.visible = true
+	bouton.visible = false
 
 func _on_Reprendre_button_up():
 	if self.visible == true :
